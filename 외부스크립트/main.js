e184e6460c931fe454b9e18945325cce90b9568e
lotteh99 <lotteh77@naver.com>
2022-02-16 23:55:32 +0900

    16page25page

--- 외부스크립트/main.js
+++ 외부스크립트/main.js
@@ -1,4 +1,4 @@
-let myName='제이펍';
+/*let myName='제이펍';
 alert(myName);
 alert('반가워요');
 const a= 10;
@@ -18,3 +18,63 @@ console.log(myString);
 let currentDate=new Date();
 console.log(currentDate);
 //let myFunction=()=> console.log('함수 확인');
+
+//숫자형 변수의 덧셈
+let number1 =10;
+let number2 =20;
+let sum = number1 + number2;
+console.log(sum);
+
+//문자열 변수의 결합
+let firstName ='브렌던';
+let familyName ='아이크';
+let fullName= firstName + familyName;
+console.log(fullName);*/
+/*
+//변수를 다른 변수에 대입
+let value1= 100;
+let value2= value1;
+console.log(value2);
+
+//상수 const 사용하기
+const number1 = 10;
+const number2 = 20;
+const sum = number1 + number2;
+console.log(sum);
+
+const familyName ='브렌던';
+const firstName = '아이크';
+const fullname = familyName + firstName;
+console.log(fullname);*/
+
+console.log('사과'=='사과');
+console.log(10<30);
+console.log(20>=30);
+
+const array1=[1,2,3];
+const array2=[1,2,3];
+console.log(array1==array2);
+
+const array3 = [1,2,3];
+const array4 = array3;
+console.log(array3==array4);
+
+let a =10;
+let b=20;
+a+=b;
+console.log(a);
+
+let c='자바';
+let d='스크립트';
+c+=d;
+console.log(c);
+
+let e=5;
+let f=2;
+e*=f;
+console.log(e);
+
+function myFunction(a) {
+    const result=a + 2;
+    return result;
+}
